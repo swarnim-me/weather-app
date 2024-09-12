8e011d457a1b14f77de5d7e76783186cedf71625
Swarnim Shresth <55665505+swarnim-me@users.noreply.github.com>
2024-09-12 22:24:43 +0530

    Basic project setup

---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require("path");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+
+module.exports = {
+	mode: "development",
+	entry: "./src/index.js",
+	output: {
+		filename: "bundle.js",
+		path: path.resolve(__dirname, "dist"),
+		clean: true,
+	},
+	devServer: {
+		watchFiles: "./src/index.html",
+	},
+	module: {
+		rules: [
+			{
+				test: /\.css$/i,
+				use: ["style-loader", "css-loader"],
+			},
+			{
+				test: /\.html$/i,
+				use: "html-loader",
+			},
+		],
+	},
+	plugins: [
+		new HtmlWebpackPlugin({
+			filename: "index.html",
+			template: "./src/index.html",
+		}),
+	],
+};
